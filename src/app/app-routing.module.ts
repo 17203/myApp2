6d60386a68b1d8f6f6127c63dad0d6ca4621e778
17203 <17203@portalcedi.edu.mx>
2025-01-09 09:24:54 -0600

    aña

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,46 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { Pag2Component } from './pag2/pag2.component';
+import { Pag3Component } from './pag3/pag3.component';
+import { Pag4Component } from './pag4/pag4.component';
+import { Pag5Component } from './pag5/pag5.component';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m =>
+      m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'pag2',
+    component: Pag2Component,
+  },
+  {
+    path: 'pag3',
+    component: Pag3Component,
+  },
+  {
+    path: 'pag4',
+    component: Pag4Component,
+  },
+  {
+    path: 'pag5',
+    component: Pag5Component,
   },
 ];
 
